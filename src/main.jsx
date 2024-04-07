69bc06d86a8d06a966b8b497d63334f3f278b0ce
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-04-07 22:32:48 +0200

    star component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from './App.jsx'
+// import './index.css'
+import StarRating from "./StarRating";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      size={24}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+  </React.StrictMode>
+);
